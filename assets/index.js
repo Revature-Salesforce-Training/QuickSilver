dc0b54cd086c1c943675ac6a51f101a5fc5f36f5
Nate Kappler <kapplernate@gmail.com>
2022-05-02 13:25:01 -0500

    Added comments and cleaned up code

--- assets/index.js
+++ assets/index.js
@@ -1,29 +1,34 @@
 /*
     Author: Nate Kappler
-    Last Edited: 5/1/22
-    Description: A sinple html page that queries an external star wars API to find a chacter and list their attributes
+    Last Edited: 5/2/22
+    Description: Javascript to implement the external API's search function and display it on the screen
 */
 
+//Defining variables and event listener 
 document.querySelector('#inputButt').addEventListener("click", search);
 let searchData = document.querySelector('#charInfo');
 let filmData = document.querySelector('#filmInfo');
 let shipData = document.querySelector('#shipInfo');
 let vehData = document.querySelector('#vehicleInfo');
+let staringln = document.querySelector('#StartLn');
 
+//Main function that will start when search button is pressed
 function search(){
     let Query = document.querySelector('#input').value
+    //Erase all html data for fresh start
+    staringln.innerHTML = '';
     searchData.innerHTML = '';
     filmData.innerHTML = '';
     shipData.innerHTML = '';
     vehData.innerHTML = '';
+    //Query external API to search for character
     let res = fetch(`https://swapi.dev/api/people/?search=${Query}`)
     .then((value) => {
         return value.json();
     })
     .then((result) => { 
-        if(result.count != 0){
+        if(result.count != 0){ //If there is a character matching the search query
             let info = document.createElement('li');
-
             info.innerHTML = "Full Name: " + result.results[0].name;
             searchData.appendChild(info);
 
@@ -56,9 +61,14 @@ function search(){
             searchData.appendChild(info);
 
 
-            //Get the films of the character
-            if(result.results[0].films != ''){
-                for(let i=0;i < result.results[0].films.length; i++){
+            //Get the films with the character in them
+            if(result.results[0].films != ''){ //If the character stars in any of the movies
+                info = document.createElement('li');
+                info.innerHTML = "Films:";
+                info.setAttribute("style", "list-style-type: none;"); //Takes away the bullet point for a nicer look
+                filmData.appendChild(info);
+
+                for(let i=0;i < result.results[0].films.length; i++){ //Iterate through the list of films and add them to the filmInfo list
                     res1 = fetch(result.results[0].films[i])
                     .then((value1) => {
                         return value1.json();
@@ -70,7 +80,7 @@ function search(){
                     });
                 }
             }
-            else{
+            else{ //If the character does NOT star in any of the movies
                 info = document.createElement('li');
                 info.innerHTML = "This character does not appear in any films"
                 filmData.appendChild(info);
@@ -78,8 +88,13 @@ function search(){
 
 
             //Get the starships associated with this character
-            if(result.results[0].starships != ''){
-                for(let i=0;i < result.results[0].starships.length; i++){
+            if(result.results[0].starships != ''){ //If the character is associated with any starships
+                info = document.createElement('li');
+                info.innerHTML = "Ships:";
+                info.setAttribute("style", "list-style-type: none;"); //Takes away the bullet point for a nicer look
+                shipData.appendChild(info);
+
+                for(let i=0;i < result.results[0].starships.length; i++){ //Iterate through the list of starships and add them to the shipInfo list
                     res1 = fetch(result.results[0].starships[i])
                     .then((value1) => {
                         return value1.json();
@@ -91,7 +106,7 @@ function search(){
                     });
                 }
             }
-            else{
+            else{ //If the character is NOT associated with any starships
                 info = document.createElement('li');
                 info.innerHTML = "This character is not associated with any starships"
                 shipData.appendChild(info);
@@ -99,8 +114,13 @@ function search(){
 
 
             //Get the vehicles associated with this character
-            if(result.results[0].vehicles != ''){
-                for(let i=0;i < result.results[0].vehicles.length; i++){
+            if(result.results[0].vehicles != ''){ //If the character is associated with any vehicles
+                info = document.createElement('li');
+                info.innerHTML = "Vehicles:";
+                info.setAttribute("style", "list-style-type: none;"); //Takes away the bullet point for a nicer look
+                vehData.appendChild(info);
+
+                for(let i=0;i < result.results[0].vehicles.length; i++){ //Iterate through the list of starships and add them to the vehicleInfo list
                     res1 = fetch(result.results[0].vehicles[i])
                     .then((value1) => {
                         return value1.json();
@@ -112,26 +132,19 @@ function search(){
                     });
                 }
             }
-            else{
+            else{ //If the character is NOT associated with any vehicles
                 info = document.createElement('li');
                 info.innerHTML = "This character is not associated with any vehicles"
                 vehData.appendChild(info);
             }
 
         }
-        else{
+        else{ //If no charcter can be found matching the search query
             searchData.innerHTML = 'Character Not found';
-            info = document.createElement('li');
-            info.innerHTML = "Films:";
-            filmData.appendChild(info);
-
-            info = document.createElement('li');
-            info.innerHTML = "Ships:";
-            shipData.appendChild(info);
-            
-            info = document.createElement('li');
-            info.innerHTML = "Vehicles:";
-            vehData.appendChild(info);
+            searchData.setAttribute("style", "position: relative;top: 20px;"); //Shifts the text down for a nicer look
+            filmData.innerHTML = '';
+            shipData.innerHTML = '';
+            vehData.innerHTML = '';
         }
     });
 
